90456612e7b9fb7821e895e32173bcb1fdde1bfc
mohankumar-markuli <mohankumarmarkuli@gmail.com>
2026-03-22 22:48:49 +0530

    complexity

--- Airtribe/01_timeSpaceComplexity/bubbleSort.js
+++ Airtribe/01_timeSpaceComplexity/bubbleSort.js
@@ -1,21 +1,28 @@
+// Bubble Sort is a simple sorting algorithm that repeatedly steps through the list, 
+// compares adjacent elements and swaps them if they are in the wrong order.
+
+// The pass through the list is repeated until the list is sorted. 
+// The algorithm gets its name from the way smaller elements "bubble" to the top of the list (beginning) 
+// while larger elements sink to the bottom (end).
+
+
 function bubbleSort(arr) {
     const n = arr.length
 
+    for (let iteration = 0; iteration < n - 1; iteration++) {
+        let madeSwap = false;
+        for (let i = 0; i < n; i++) {
 
-    for(let iteration=0; iteration<n-1;iteration++){
-    let madeSwap = false;
-        for(let i=0;i<n;i++){
-
-            if(i+1 < n && arr[i]>arr[i+1]){
+            if (i + 1 < n && arr[i] > arr[i + 1]) {
 
                 madeSwap = true;
                 let temp = arr[i]
-                arr[i] = arr[i+1]
-                arr[i+1] = temp
+                arr[i] = arr[i + 1]
+                arr[i + 1] = temp
             }
         }
-        if(!madeSwap) break;
-        console.log({iteration,arr})
+        if (!madeSwap) break;
+        console.log({ iteration, arr })
     }
     return arr
 }
--- Airtribe/01_timeSpaceComplexity/linearSearch.js
+++ Airtribe/01_timeSpaceComplexity/linearSearch.js
@@ -1,3 +1,6 @@
+// linerar search is a simple searching algorithm that checks each element in the array sequentially 
+// until the desired element is found or the end of the array is reached.
+
 function search(arr,key){
     n = arr.length
     for(let i=0; i<n; i++){
