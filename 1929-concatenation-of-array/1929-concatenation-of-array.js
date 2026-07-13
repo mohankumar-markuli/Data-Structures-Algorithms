1b172ddf694b6401aa91887ca7ef36fbbbadfa65
Mohankumar <mohankumarmarkuli@gmail.com>
2026-07-13 23:22:58 +0530

    Time: 0 ms (100%), Space: 56.4 MB (94.58%) - LeetHub

---
+++ 1929-concatenation-of-array/1929-concatenation-of-array.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var getConcatenation = function(nums) {
+    const length = nums.length
+    for (let i=0;i<length;i++){
+        nums.push(nums[i])
+    }
+    return nums;
+};
